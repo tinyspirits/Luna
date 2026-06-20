c18ca2f9508d78ac187e27910c412ab55f3aed57
Tiny <tinyspirit99@gmail.com>
2026-06-20 10:30:39 +0700

    feat: initialize Capacitor configuration and add native dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tinyspirits.luna',
+  appName: 'Luna',
+  webDir: 'dist'
+};
+
+export default config;
